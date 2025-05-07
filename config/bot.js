5b1a881ee4fecadedb1c64b57bf41f7f92d39199
deepcharger <github-deep.pp322@slmails.com>
2025-05-07 13:56:40 +0200

    Update bot.js

--- config/bot.js
+++ config/bot.js
@@ -1,7 +1,6 @@
 // Configurazione del bot Telegram
 const { Telegraf, Scenes, session } = require('telegraf');
-const sellAnnouncementScene = require('../handlers/scenes/sellAnnouncement');
-const buyKwhScene = require('../handlers/scenes/buyKwh');
+const logger = require('../utils/logger');
 
 // Configurazione delle variabili d'ambiente
 const BOT_TOKEN = process.env.BOT_TOKEN;
@@ -13,16 +12,35 @@ const LOG_TO_FILE = process.env.LOG_TO_FILE === 'true';
 // Inizializza il bot Telegram
 const bot = new Telegraf(BOT_TOKEN);
 
-// Configura le scene per i wizard
-const stage = new Scenes.Stage([
-  sellAnnouncementScene,
-  buyKwhScene
-]);
+// Per evitare le dipendenze circolari, spostiamo l'importazione delle scene
+// dopo l'esportazione del modulo
+let stage = null;
+
+// Funzione per inizializzare le scene
+const initScenes = () => {
+  if (stage === null) {
+    // Importa le scene qui, dopo l'esportazione del modulo
+    const sellAnnouncementScene = require('../handlers/scenes/sellAnnouncement');
+    const buyKwhScene = require('../handlers/scenes/buyKwh');
+    
+    // Configura le scene per i wizard
+    stage = new Scenes.Stage([
+      sellAnnouncementScene,
+      buyKwhScene
+    ]);
+    
+    logger.debug('Scene inizializzate');
+  }
+  
+  return stage;
+};
 
 // Esporta le configurazioni
 module.exports = {
   bot,
-  stage,
+  get stage() {
+    return initScenes();
+  },
   BUY_GROUPS_CONFIG,
   SELL_GROUPS_CONFIG,
   LOG_LEVEL,
