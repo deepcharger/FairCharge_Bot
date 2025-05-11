2a804b05a8cb1add5a9487e2e57a941c5f4a3d7e
deepcharger <github-deep.pp322@slmails.com>
2025-05-11 12:38:32 +0200

    Update user.js

--- models/user.js
+++ models/user.js
@@ -14,7 +14,18 @@ const userSchema = new mongoose.Schema({
     sell: { type: String, ref: 'Announcement', default: null },
     buy: { type: String, ref: 'Announcement', default: null }
   },
-  transactions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Transaction' }]
+  transactions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Transaction' }],
+  // Nuovo campo per il portafoglio dettagliato
+  walletDetails: {
+    walletLastUpdated: { type: Date },
+    partners: [{ 
+      partnerId: { type: Number, required: true },
+      partnerName: String,
+      totalTransactions: { type: Number, default: 0 },
+      kwhExchanged: { type: Number, default: 0 },
+      lastTransactionDate: { type: Date }
+    }]
+  }
 });
 
 /**
@@ -37,6 +48,51 @@ userSchema.methods.isTrustedSeller = function() {
   const percentage = this.getPositivePercentage();
   // Venditore affidabile se ha almeno 90% di feedback positivi e almeno 5 recensioni
   return percentage !== null && percentage >= 90 && this.totalRatings >= 5;
+};
+
+/**
+ * Aggiorna i dettagli del portafoglio con un partner
+ * @param {Number} partnerId - ID del partner
+ * @param {String} partnerName - Nome del partner
+ * @param {Number} kwhAmount - Quantità di kWh scambiati
+ * @returns {Promise<void>}
+ */
+userSchema.methods.updateWalletPartner = async function(partnerId, partnerName, kwhAmount) {
+  // Inizializza il wallet se non esiste
+  if (!this.walletDetails) {
+    this.walletDetails = {
+      walletLastUpdated: new Date(),
+      partners: []
+    };
+  }
+  
+  // Cerca il partner esistente
+  let partner = this.walletDetails.partners.find(p => p.partnerId === partnerId);
+  
+  // Se non esiste, lo crea
+  if (!partner) {
+    partner = {
+      partnerId: partnerId,
+      partnerName: partnerName,
+      totalTransactions: 0,
+      kwhExchanged: 0,
+      lastTransactionDate: new Date()
+    };
+    this.walletDetails.partners.push(partner);
+  } 
+  // Altrimenti, aggiorna i dettagli
+  else {
+    partner.partnerName = partnerName;
+    partner.totalTransactions += 1;
+    partner.kwhExchanged += kwhAmount;
+    partner.lastTransactionDate = new Date();
+  }
+  
+  // Aggiorna il timestamp dell'ultimo aggiornamento
+  this.walletDetails.walletLastUpdated = new Date();
+  
+  // Salva le modifiche
+  return this.save();
 };
 
 // Crea il modello User
